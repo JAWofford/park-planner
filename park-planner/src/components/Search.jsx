aec06c7663bd0636269861b48534f903e4d4e86b
JWofford <wofford.sj@gmail.com>
2026-03-23 15:46:54 -0500

    Added managment of parksData state and select state field

--- park-planner/src/components/Search.jsx
+++ park-planner/src/components/Search.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react';
+import parksData from '../data/parksData';
 
 export default function Search() {
+  
+  const [parks] = useState(parksData);
+    const [selectedState, setSelectedState] = useState('');
+
+  const states = [
+    ...new Set(
+      parks.flatMap(park => park.states.split(','))
+    )
+  ].sort();
+  
   return (
-    <div>
-        <h1>Search Page</h1>
-        <h3>call results component from here</h3>
+    
+  <div>
+    <div className="search-banner">
+      <h2>Find Your Next Adventure</h2>
+      <p>Search 400+ national parks, monuments, and recreation areas</p>
+      
+        <select
+          value={selectedState}
+          onChange={(evt) => setSelectedState(evt.target.value)}
+        >
+          <option value="">All States</option>
+
+          {states.map((state) => (
+            <option key={state} value={state}>
+              {state}
+            </option>
+          ))}
+        </select>
+      
     </div>
+  </div>
   )
 }
